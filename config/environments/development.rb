b1e9e58c23054de6fffd266b0046fb966a6bd1df
Lenin Luque <xleninx@gmail.com>
2015-08-01 02:12:33 -0300

    add config for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,17 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_MAILER"],
+    authentication: "plain",
+    user_name: ENV["USERNAME_MAILER"],
+    password: ENV["PASSWORD_MAILER"]
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
