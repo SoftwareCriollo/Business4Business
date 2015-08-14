5d487003fa55a0cb64c41809dbc850682fa1aab7
Lenin Luque <xleninx@gmail.com>
2015-08-13 22:44:06 -0300

    add draper for create new decorator for projects and companies

---
+++ app/decorators/project_decorator.rb
@@ -0,0 +1,19 @@
+class ProjectDecorator < Draper::Decorator
+  delegate_all
+
+  def skills
+    helpers.content_tag :span, class: 'time' do
+      object.skills.map(&:name).join(' | ')
+    end
+  end
+
+  def company_name
+    object.company.name
+  end
+
+  def description
+    object.description.truncate(80)
+  end
+
+
+end
