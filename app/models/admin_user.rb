5d11fecfe88eb1851ddedd69597ba8a4d96a5811
Lenin Luque <lenin@softwarecriollo.com>
2015-08-19 13:23:48 -0300

    fix after sign in path for admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,11 @@
 class AdminUser < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def dashboard_path
+  def after_sign_in_path
     admin_root_path
   end
 end
