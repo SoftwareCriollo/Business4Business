4f623f5d1a5f44868997ebe704c6c9d0d4cf3951
Lenin Luque <lenin@softwarecriollo.com>
2015-08-24 16:05:06 -0300

    Fix ability for admin user

--- app/models/ability_admin.rb
+++ app/models/ability_admin.rb
@@ -3,10 +3,7 @@ class AbilityAdmin
 
   def initialize(user)
     user ||= AdminUser.new # guest user (not logged in)
-    puts '-'*50
-    puts user.class.name
-    puts '-'*50
-    if user.class.name === 'AdminUser'
+    if user.class.name == 'AdminUser'
       can :manage, :all
     else
       can :read, :all
